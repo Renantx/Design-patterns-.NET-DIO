0bff1bae3327fb6ecc126508c8b0b63be0fe366a
Renantx <renan.tkdunb@gmail.com>
2021-08-22 10:33:13 -0300

    commit inicial

---
+++ Localiza.Frotas.Infra/Repository/EF/FrotaContext.cs
@@ -0,0 +1,15 @@
+﻿using Localiza.Frotas.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Localiza.Frotas.Infra.Repository.EF
+{
+    public class FrotaContext : DbContext
+    {
+        public FrotaContext(DbContextOptions<FrotaContext> options)
+           : base(options)
+        {
+        }
+
+        public DbSet<Veiculo> Veiculos { get; set; }
+    }
+}
